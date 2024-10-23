b5f2295b9ce102630b24e4fbc6d87dcc614c338e
mitchsaah <71660816+mitchsaah@users.noreply.github.com>
2024-10-23 13:23:20 +0200

    Added Xcode project files

---
+++ Challenge-2/Challenge-2/Challenge_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge_2App.swift
+//  Challenge-2
+//
+//  Created by Mitch Saah on 23/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Challenge_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

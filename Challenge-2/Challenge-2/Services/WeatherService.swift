96e20ead6baac3c3f0b7e4ced9f4af794c7f9368
mitchsaah <71660816+mitchsaah@users.noreply.github.com>
2024-11-13 10:34:35 +0100

    Coordinates for Liedekerke Bos (Tracking this soil)

--- Challenge-2/Challenge-2/Services/WeatherService.swift
+++ Challenge-2/Challenge-2/Services/WeatherService.swift
@@ -3,4 +3,10 @@ import Alamofire
 class WeatherService {
     private let apiKey = "e44c5b1dfd503e307697d439de360179"
     private let baseURL = "https://api.openweathermap.org/data/2.5/onecall"
+    
+    // Coordinates for Liedekerke Bos
+    private let defaultLat: Double = 50.8899
+    private let defaultLon: Double = 4.0966
+    
+    
 }

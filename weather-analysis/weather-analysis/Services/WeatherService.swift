95335411f824015ed2048ebf79bece7c1ba8b1dd
mitchsaah <71660816+mitchsaah@users.noreply.github.com>
2024-12-28 18:23:39 +0100

    Added a fetchDailyWeather function

--- weather-analysis/weather-analysis/Services/WeatherService.swift
+++ weather-analysis/weather-analysis/Services/WeatherService.swift
@@ -1,6 +1,10 @@
 import Foundation
+import Alamofire
 
 class WeatherService {
     private let apiKey = "e44c5b1dfd503e307697d439de360179"
     private let baseURL = "https://api.openweathermap.org/data/2.5/onecall"
+    
+    func fetchDailyWeather(lat: Double, lon: Double, completion: @escaping ([DailyWeather]?) -> Void) {
+    }
 }

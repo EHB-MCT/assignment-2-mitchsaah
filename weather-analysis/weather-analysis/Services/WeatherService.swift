117ac1af1f349a49143224db69e5107090b3f307
mitchsaah <71660816+mitchsaah@users.noreply.github.com>
2024-12-28 19:34:27 +0100

    Implemented the Alamofire request in fetchDailyWeather

--- weather-analysis/weather-analysis/Services/WeatherService.swift
+++ weather-analysis/weather-analysis/Services/WeatherService.swift
@@ -6,5 +6,22 @@ class WeatherService {
     private let baseURL = "https://api.openweathermap.org/data/2.5/onecall"
     
     func fetchDailyWeather(lat: Double, lon: Double, completion: @escaping ([DailyWeather]?) -> Void) {
+        let parameters: [String: Any] = [
+                    "lat": lat,
+                    "lon": lon,
+                    "exclude": "minutely,hourly,current,alerts",
+                    "units": "metric",
+                    "appid": apiKey
+        ]
+
+        AF.request(baseURL, parameters: parameters).responseDecodable(of: WeatherResponse.self) { response in
+            switch response.result {
+            case .success(let data):
+                completion(data.daily)
+            case .failure(let error):
+                print("Error fetching weather data: \(error)")
+                completion(nil)
+            }
+        }
     }
 }

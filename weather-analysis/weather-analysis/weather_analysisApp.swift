c13b47f38fc4046141b74759237f001ecf9ad38c
mitchsaah <71660816+mitchsaah@users.noreply.github.com>
2024-12-28 15:45:59 +0100

    Inital Commit - Xcode project - This is for the implementation of Xcode for this project.

---
+++ weather-analysis/weather-analysis/weather_analysisApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_analysisApp.swift
+//  weather-analysis
+//
+//  Created by Mitch Saah on 28/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct weather_analysisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

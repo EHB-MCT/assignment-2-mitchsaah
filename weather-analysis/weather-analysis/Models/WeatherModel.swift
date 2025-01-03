bba52b8fc766920801eb66df7b8e669b493ca1d0
mitchsaah <71660816+mitchsaah@users.noreply.github.com>
2025-01-03 13:42:16 +0100

    Updated the Model to Include alert data I decided to track the weather conditions when the user allows the app (for final work) to use their location, that I'll be giving out alerts based on the current weather: - Extended the DailyWeather model to include Temperature, rain, pop, and uvi

--- weather-analysis/weather-analysis/Models/WeatherModel.swift
+++ weather-analysis/weather-analysis/Models/WeatherModel.swift
@@ -6,6 +6,10 @@ struct WeatherResponse: Codable {
 
 struct DailyWeather: Codable {
     let dt: TimeInterval
+    let temp: Temperature
+    let pop: Double  // Probability
+    let rain: Double?
+    let uvi: Double
     
     struct Temperature: Codable {
             let min: Double

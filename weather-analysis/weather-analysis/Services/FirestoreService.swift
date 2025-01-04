1edc183497d61d084b238a09a2a51112e5ebcfc5
mitchsaah <71660816+mitchsaah@users.noreply.github.com>
2025-01-04 12:57:53 +0100

    Added ISO8601 date formatting

--- weather-analysis/weather-analysis/Services/FirestoreService.swift
+++ weather-analysis/weather-analysis/Services/FirestoreService.swift
@@ -6,7 +6,8 @@ class FirestoreService {
     
     // Saves an alert to Firesstore
     func saveWeatherAlert(date: TimeInterval, alertDetails: [String: Any], completion: @escaping (Error?) -> Void) {
-        // Placeholder for saving logic
+        let formattedDate = ISO8601DateFormatter().string(from: Date(timeIntervalSince1970: date)) // Src: Apple Developer
+        print("Formatted Date: \(formattedDate)")
         completion(nil)
     }
 }

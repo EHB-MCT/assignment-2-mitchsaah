3addb14d35273cbc871e6992ac66a01f70e34b80
mitchsaah <71660816+mitchsaah@users.noreply.github.com>
2025-01-04 13:20:54 +0100

    Implemented Firestore writing logic

--- weather-analysis/weather-analysis/Services/FirestoreService.swift
+++ weather-analysis/weather-analysis/Services/FirestoreService.swift
@@ -7,7 +7,8 @@ class FirestoreService {
     // Saves an alert to Firesstore
     func saveWeatherAlert(date: TimeInterval, alertDetails: [String: Any], completion: @escaping (Error?) -> Void) {
         let formattedDate = ISO8601DateFormatter().string(from: Date(timeIntervalSince1970: date)) // Src: Apple Developer
-        print("Formatted Date: \(formattedDate)")
-        completion(nil)
+        db.collection("weatherAlerts").document(formattedDate).setData(alertDetails) { error in
+            completion(error)
+        }
     }
 }
